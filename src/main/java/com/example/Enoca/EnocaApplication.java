7e4ccc1a14fc614c6670a52f33849f4dbfa3abe2
duyguasena <duyguasenakaya@gmail.com>
2024-11-05 14:21:28 +0300

    init project

---
+++ src/main/java/com/example/Enoca/EnocaApplication.java
@@ -0,0 +1,12 @@
+package com.example.Enoca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class EnocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnocaApplication.class, args);
+	}
+
+}
